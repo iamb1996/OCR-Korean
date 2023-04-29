c71b3e5095374b8e9239b71982689321ee24b9d5
iamb1996 <124360631+iamb1996@users.noreply.github.com>
2023-04-29 21:33:19 +0200

    Update code.py

--- .github/workflows/code.py
+++ .github/workflows/code.py
@@ -1,9 +1,12 @@
-def hello():
-    print("hi")
+import os
 
+def main():
+  print("Hello from GitHub Actions!")
+  token = os.environ.get("AZURE_SECRET_TOKEN")
+  if not token:
+    raise RuntimeError("AZURE_SECRET_TOKEN env var is not set!")
+  print("All good! we found our env var")
+  
 
-def bye():
-    print("bye")
-
-
-print(hello())
+if __name__ == '__main__':
+  main()
